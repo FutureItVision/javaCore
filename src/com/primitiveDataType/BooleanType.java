cb3575dcd45ccc2e52d0c39b1fc80e6185279457
nadiruzzamanmuhammad <nadiruzzamanqa@gamil.com>
2020-12-20 09:06:06 -0500

    We added int and boolean class

---
+++ src/com/primitiveDataType/BooleanType.java
@@ -0,0 +1,10 @@
+package com.primitiveDataType;
+
+public class BooleanType {
+
+    static boolean bangladeshIndependent=true;
+    static boolean americaCloseToDhaka=false;
+    public static void main(String[]args){
+        System.out.println(americaCloseToDhaka);
+    }
+}
---
+++ src/com/primitiveDataType/IntDataType.java
@@ -0,0 +1,19 @@
+package com.primitiveDataType;
+
+public class IntDataType {
+
+  static int banana=2;
+    static int lemon=3;
+    static int TotalPrice=banana+lemon;
+   static String Memo ="Total Price: $";
+
+    static String total ="Total Price: $";
+
+
+    public static void main(String[]args){
+        System.out.println(total+TotalPrice);
+    }
+
+
+
+}
