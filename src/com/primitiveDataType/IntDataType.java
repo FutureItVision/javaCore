a3ca2ff8095d93eaa41a068d98da37d8da91854f
nadiruzzamanmuhammad <nadiruzzamanqa@gamil.com>
2020-12-27 08:57:03 -0500

    Regular method and method overloading

--- src/com/primitiveDataType/IntDataType.java
+++ src/com/primitiveDataType/IntDataType.java
@@ -2,18 +2,27 @@ package com.primitiveDataType;
 
 public class IntDataType {
 
-  static int banana=2;
+
+    static int banana=2;
     static int lemon=3;
     static int TotalPrice=banana+lemon;
-   static String Memo ="Total Price: $";
+    static String Memo ="Total Price: $";
 
     static String total ="Total Price: $";
 
 
     public static void main(String[]args){
+        grocery();
+
+    }
+
+    public static void grocery(String fish){
         System.out.println(total+TotalPrice);
     }
 
+    public static void grocery(){
+        System.out.println(total+TotalPrice);
+    }
 
 
 }
